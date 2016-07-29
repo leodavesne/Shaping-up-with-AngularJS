41de2ab587b9c0bd156a26ca646c77e0044796ef
Léo Davesne <leo@davesne.net>
2016-07-29 15:12:55 +0200

    1.5 Built-in Directives

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 	
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: '. . .',
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: '. . .',
+			canPurchase: true
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: '. . .',
+			canPurchase: true
+		},
+	];
 })();
